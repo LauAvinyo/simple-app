bcc75aa17f1c5c6b533bbffa6e2a918d9c9dbc64
Laura  Avinyo Esteban <lauraavinyoesteban@Lauras-MacBook-Pro.local>
2022-09-06 17:09:04 +0200

    Fix use(component) after mount bug

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 </script>
 
 <template>
-  <Button label="label"></Button>
+  <Button label="button 1" > </Button>
+  <Button label="button 2" > </Button>
 </template>
 
 <style scoped>
-
+  
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Button from 'poc-lib'
-import "poc-lib/dist/style.css"
+import Button from '@lauavinyo/poc-lib'
+import "@lauavinyo/poc-lib/dist/style.css"
 
 import './assets/main.css'
 
 const app = createApp(App)
-app.mount('#app')
+
 app.use(Button)
+app.mount('#app')
